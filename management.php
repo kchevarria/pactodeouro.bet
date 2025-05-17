6c4dfdc51c5a00e6ab08289022b7ed106bb8415e
kchevarria <kgarciachevarria@gmail.com>
2025-05-17 17:45:15 -0300

    Update management.php

--- management.php
+++ management.php
@@ -74,26 +74,29 @@ if (!isset($_SESSION['logado']) || $_SESSION['logado'] !== true) {
             <div class="card-footer d-flex justify-content-end align-items-center">
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    <i class="bi bi-plus pe-2"></i> Nova plataforma
+                    <i class="bi bi-plus-lg pe-2"></i> Nova plataforma
                 </button>
             </div>
         </div>
     </div>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="cadastroPlataforma" tabindex="-1" aria-labelledby="cadastroPlataformaLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="cadastroPlataformaLabel">Cadastro de Plataforma</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <input type="text" name="" id="" class="form-control">
+                    <input type="number" name="" id="" class="form-control">
+                    <input type="text" name="" id="" class="form-control">
+                    <input type="file" name="" id="" class="form-control">
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Descartar</button>
+                    <button type="button" class="btn btn-primary">Salvar</button>
                 </div>
             </div>
         </div>
