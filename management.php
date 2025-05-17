356303cefd2befb643cfd773b78fa33db9eb50c9
kchevarria <kgarciachevarria@gmail.com>
2025-05-17 13:13:18 -0300

    controle de sessao
    removido para criacao do primeiro user

--- management.php
+++ management.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-if (!isset($_SESSION['usuario_id'])) {
-    header("Location: login.html");
-    exit;
-}
+// session_start();
+// if (!isset($_SESSION['usuario_id'])) {
+//     header("Location: login.html");
+//     exit;
+// }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
